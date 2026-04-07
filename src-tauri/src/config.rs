9a56dab1d24cfa96cd6c8475ac389b22c7df7286
ly773615199 <773615199@qq.com>
2026-04-07 14:34:27 +0800

    Add src-tauri/src/config.rs

---
+++ src-tauri/src/config.rs
@@ -0,0 +1,6 @@
+pub const ALIST_PORT: u16 = 5244;
+pub const BORE_HOST: &str = "bore.pub";
+pub const STARTUP_TIMEOUT_SECS: u64 = 15;
+pub const PASSWORD_FILE: &str = "alist_password.json";
+pub const ALIST_BINARY: &str = "alist.exe";
+pub const BORE_BINARY: &str = "bore.exe";
